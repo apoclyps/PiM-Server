326d685fc1e050110d1c23f4dfb1823330aa6bab
apoclyps <kyle90adam@hotmail.com>
2014-03-04 10:44:08 +0000

    Asylum web scrapper refactored to allow for easier maintance of the code. Additional work is required however for now it's an improvement over the previous method.
    A testcase has also been created to ensure it works correctly.
    
    The constructor has also been paramaterised to allow for selection of
    ranges in which to scrape the Asylum website.

---
+++ PIM-Server/src/uk/co/kyleharrison/pim/service/scrapper/AsylumService.java
@@ -0,0 +1,232 @@
+package uk.co.kyleharrison.pim.service.scrapper;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import uk.co.kyleharrison.pim.storage.mysql.MySQLFacade;
+
+/**
+ * @author apoclyps
+ *
+ */
+public class AsylumService {
+
+	private int totalPages = 5005;
+	private int pageCounter = 550;
+	private int pageStop = 1000;
+	private int pagesSaved = 0;
+	private int nextRequestDelay = 5000;
+	private MySQLFacade mySQLFacade = new MySQLFacade();
+	private int count = 0;
+	private boolean priceFlag;
+	private boolean detailsFlag;
+	private boolean quantityFlag;
+	private Document doc;
+	private Map<String, String> entry;
+
+	public AsylumService() {
+		super();
+		this.priceFlag = false;
+		this.detailsFlag = false;
+		this.quantityFlag = false;
+		this.entry = new HashMap<String, String>();
+	}
+	
+	public AsylumService(int start, int end) {
+		super();
+		this.priceFlag = false;
+		this.detailsFlag = false;
+		this.quantityFlag = false;
+		this.entry = new HashMap<String, String>();
+		this.pageCounter=start;
+		this.pageStop = end;
+	}
+
+	private Document generateDocument() throws IOException{
+		return doc = Jsoup.connect("http://www.asylum-booksandgames.com/shop/index.php?page="+ pageCounter+ "&searchStr=&act=viewCat&Submit=Go").get();
+	}
+	
+	private void parseHTML(Elements columns){
+		this.entry = new HashMap<String, String>();
+
+		for (Element column : columns) {
+
+			if (column.text().startsWith("Buy") || column.text().startsWith("More")) {
+				praseIDQuantity(column);
+			} else if (column.text().length() <= 8) {
+				parseCost(column);
+			} else if (column.text().equals("Description")) {
+				// Ignore and reset
+				count = 0;
+			} else {
+				System.out.println("\n" + "Description : \t"+ column.text());
+				// pass column text
+				parseDescription(column);
+			}
+			if (priceFlag && quantityFlag && detailsFlag) {
+
+				System.out.println("Inserting Record "+ this.entry.get("id") + " : Page " + this.pageCounter);
+				boolean inserted = mySQLFacade.insertAsylumRecord(this.entry);
+				System.out.println("Success : " + inserted);
+
+				this.entry = new HashMap<String, String>();
+				this.priceFlag = false;
+				this.quantityFlag = false;
+				this.detailsFlag = false;
+			}
+		}
+	}
+
+	private void praseIDQuantity(Element column){
+		try {
+			Element quantityInput = column.select("input[type=hidden]").get(1);
+			String quantity = quantityInput.attr("value");
+			// System.out.println("Quantity : " + quantity);
+			this.entry.put("quantity", quantity);
+
+			Element idInput = column.select("input[type=hidden]").get(0);
+			String id = idInput.attr("value");
+			// System.out.println("Product ID : " + id);
+			this.entry.put("id", id.toString());
+
+			// System.out.println("id test"+ entry.get("id"));
+			this.quantityFlag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void parseCost(Element column){
+		try {
+			double value = Double.parseDouble(column.text());
+			// System.out.println("Cost £ : " + value);
+			this.entry.put("cost", Double.toString(value));
+			priceFlag = true;
+		} catch (Exception e) {
+
+		}
+	}
+	
+	private void parseDescription(Element column){
+		// Split Strings
+		String[] name = null;
+		String[] publisher = null;
+		String[] issue = null;
+		String volume = null;
+
+		try {
+			String[] splitString = column.text().split(
+					"Title: ");
+			try {
+				name = splitString[1].split("Publisher: ");
+				this.entry.put("title", name[0]);
+				try {
+					publisher = name[1].split("Issue Number: ");
+					this.entry.put("publisher", publisher[0]);
+					try {
+						issue = publisher[1].split("Volume: ");
+						this.entry.put("issue", issue[0]);
+						try {
+							volume = issue[1];
+							this.entry.put("volume", volume);
+						} catch (Exception e) {
+							volume = null;
+							System.out
+									.println("Volume exception");
+							this.entry.put("volume", volume);
+							e.printStackTrace();
+						}
+					} catch (Exception e) {
+						issue[0] = null;
+						System.out.println("issue exception");
+						this.entry.put("issue", "null");
+						e.printStackTrace();
+					}
+				} catch (Exception e) {
+					publisher[0] = null;
+					System.out.println("publisher exception");
+					this.entry.put("publisher", "null");
+					e.printStackTrace();
+				}
+			} catch (Exception e) {
+				// System.out.println("split "+column.text());
+				// name = column.text().
+				Element title = column.select("strong").first();
+				// System.out.println("Title" + title.text());
+				// name[0] = title.text();
+				this.entry.put("title", title.text());
+				// e.printStackTrace();
+			}
+			// Save strings
+			System.out.println("Title : \t"+ this.entry.get("title"));
+			System.out.println("Issue Number : \t"+ this.entry.get("issue"));
+			System.out.println("Publisher : \t"+ this.entry.get("publisher"));
+			System.out.println("Volume : \t"+ this.entry.get("volume"));
+
+			// Sub title, author, format
+			detailsFlag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		}
+	
+	/**
+	 * Returns an Image object that can then be painted on the screen. 
+	 * The url argument must specify an absolute {@link URL}. The name
+	 * argument is a specifier that is relative to the url argument. 
+	 * <p>
+	 *
+	 * @param  url  an absolute URL giving the base location of the image
+	 * @param  name the location of the image, relative to the url argument
+	 * @return      the image at the specified URL
+	 * @see         Image
+	 */
+	public boolean executeQuery() {
+		do {
+			try {
+				Document doc = generateDocument();
+
+				Element table = doc.select("table").get(1);
+				Elements rows = table.select("tr");
+				Elements columns = rows.select("td");
+
+				parseHTML(columns);
+
+				pageCounter++;
+				System.out.println("Increment counter : " + pageCounter);
+
+				Thread.sleep(nextRequestDelay);
+			} catch (SocketTimeoutException e) {
+				// pageCounter--;
+				System.out.println("Time Out : Page " + pageCounter);
+				try {
+					Thread.sleep(nextRequestDelay);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.out.println("Failed on page counter : " + pageCounter);
+				System.out.println("Program Stopped");
+				return false;
+				//break;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				System.out.println("Failed on page counter : " + pageCounter);
+				System.out.println("Program Stopped");
+				return false;
+				//break;
+			}
+
+		} while (pageCounter <= pageStop);
+		System.out.println("Program Completed");
+		return true;
+	}
+}
---
+++ PIM-Server/src/uk/co/kyleharrison/test/pim/AsylumServiceTest.java
@@ -0,0 +1,32 @@
+package uk.co.kyleharrison.test.pim;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import uk.co.kyleharrison.pim.service.scrapper.AsylumService;
+
+public class AsylumServiceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void constructor() {
+		AsylumService as = new AsylumService(550,551);
+		assert(!as.equals(null));
+	}
+	
+	@Test
+	public void executeQuery(){
+		AsylumService as = new AsylumService(550,551);
+		boolean success = as.executeQuery();
+		assert(success==true);
+	}
+
+}
